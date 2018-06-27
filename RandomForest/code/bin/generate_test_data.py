def217d2e808779ae461c5bb1f7b38c79f80aa2a
Tommy Tracy II <tjt7a@virginia.edu>
2018-06-27 11:30:41 -0400

    Added functionality for generating new testing data and updated README with new features

--- RandomForest/code/bin/generate_test_data.py
+++ RandomForest/code/bin/generate_test_data.py
@@ -7,12 +7,16 @@ from optparse import OptionParser
 if __name__ == '__main__':
 
     # Parse Command Line Arguments
-    usage = '%prog [options]'
+    usage = '%prog [options] input filename'
 
     parser = OptionParser(usage)
 
     parser.add_option('--short', action='store_true', default=False, dest='short',
                       help='Make a short version of the input (100 samples)')
+
+    parser.add_option('-o', '--input_filename', type='string', dest='input_filename',
+                      default='input_file',
+                      help='Generate input filename')
 
     parser.add_option('--input_format', type='string', dest='input_format',
                       default='BIN',
@@ -30,10 +34,16 @@ if __name__ == '__main__':
 
     options, args = parser.parse_args()
 
-    X_test, y_test = load_test("testing_data.pickle")
+    input_filename = "testing_data.pickle"
+
+    if len(args) == 1:
+
+        input_filename = args[0]
+
+    X_test, y_test = load_test(input_filename)
     ft = load_feature_table("feature_table.pickle")
 
-    ft.input_file(X_test, "input_file", onebased=False,
+    ft.input_file(X_test, options.input_filename, onebased=False,
                   short=options.short, delimited=True)
 
     #np.savetxt("testing.bin", X_test[:size], delimiter=',', fmt='%d')
